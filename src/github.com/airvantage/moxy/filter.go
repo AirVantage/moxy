83622b9195ba989b9817c43cdef963bd412d765e
Manuel Sangoi <msangoi@sierrawireless.com>
2015-06-03 15:47:30 +0200

    Fix typo

--- src/github.com/airvantage/moxy/filter.go
+++ src/github.com/airvantage/moxy/filter.go
@@ -1,6 +1,6 @@
 package moxy
 
-// MqqtFilter is fitering an incoing uplink or downlink MQTT packet, by generating a new packet (or just proxyfying the received one,
+// MqttFilter is fitering an incoming uplink or downlink MQTT packet, by generating a new packet (or just proxyfying the received one,
 // and possibly send backway a packet
 type MqttFilter interface {
 	Filter(inPacket []byte, uplink bool, metadata map[string]interface{}) (outPacket []byte, backward []byte)
